a673fefbb2fcc3f10348ff246cb119c695e25b0c
Théis Bazin <theis.bazin@mwm.io>
2024-05-06 11:41:14 +0200

    style: :recycle: minor refactor

--- run_poetry_completions_init.sh
+++ run_poetry_completions_init.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
-## HACK(@theis, 2024/05/06): should use $ZSH_CUSTOM as advocated by https://python-poetry.org/docs/#oh-my-zsh
-# but it is not initialized when running this script
-# mkdir $ZSH_CUSTOM/plugins/poetry
 if
   command -v poetry 1>/dev/null 2>/dev/null
 then
+  ## HACK(@theis, 2024/05/06): should use $ZSH_CUSTOM as advocated by https://python-poetry.org/docs/#oh-my-zsh
+  # but it is not initialized when running this script
+  # mkdir $ZSH_CUSTOM/plugins/poetry
   mkdir -p ~/.oh-my-zsh/custom/plugins/poetry
   ## HACK(@theis, 2024/05/06): should use $ZSH_CUSTOM as advocated by https://python-poetry.org/docs/#oh-my-zsh
   # but it is not initialized when running this script
